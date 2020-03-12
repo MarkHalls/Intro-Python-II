813ee9659396bed90c277c7df9320c839601bb1b
Mark Halls <mark.halls@gmail.com>
2020-03-12 15:09:41 -0700

    Implement setters and getters for items

--- src/room.py
+++ src/room.py
@@ -1,3 +1,6 @@
+from item import Item
+from print_wrap import print_wrap
+
 # Implement a class to hold room information. This should have name and
 # description attributes.
 class Room:
@@ -22,6 +25,48 @@ class Room:
         self.s_to = None
         self.w_to = None
         self.e_to = None
+        self.items = list()
+
+    def __str__(self):
+        room_details = [
+            "****************************",
+            f"Room: {self.name}",
+            " ",
+            "Details:",
+            f"{self.description}",
+            " ",
+        ]
+
+        for line in room_details:
+            print_wrap(line)
+
+        # show list of items in the room
+        if len(self.items) > 0:
+            print_wrap("You see some items")
+            for item in self.items:
+                print_wrap(item.name)
+
+        # get directions and print them to the screen
+        for line in self.get_directions():
+            print_wrap(line)
+
+        return ""
+
+    def take_item(self, item_name):
+        for item in self.items:
+            if item.name == item_name:
+                self.items.remove(item)
+                return item
+            else:
+                print_wrap(f"You searched the room but couldn't find {item_name}")
+
+    def get_items(self):
+        for item in self.items:
+            print_wrap(item.name)
+
+    def set_items(self, item):
+        if isinstance(item, Item):
+            self.items.append(item)
 
     def get_directions(self):
         compass = {
@@ -29,7 +74,6 @@ class Room:
             "s_to": "south",
             "e_to": "east",
             "w_to": "west",
-            "e_to": "east",
         }
         directions = []
 
@@ -40,6 +84,7 @@ class Room:
         direction_string = str(directions).strip("[]").replace("'", "")
 
         return [
+            " ",
             f"There are rooms to the {direction_string}",
             " ",
             "  N  ",
