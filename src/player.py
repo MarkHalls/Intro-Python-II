4800edbd495f9269e1eadd7408746c3b1819a7f8
Mark Halls <mark.halls@gmail.com>
2020-03-12 01:09:57 -0700

    Add Player class with constructor and move method

--- src/player.py
+++ src/player.py
@@ -1,2 +1,21 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player:
+    """
+    The Player object contains details about the player
+
+    Args: 
+        name (str): The players name
+        current_room (Room): The room the player is currently in
+    """
+
+    def __init__(self, name, current_room):
+        self.name = name
+        self.current_room = current_room
+
+    def move(self, newRoom):
+        if newRoom:
+            self.current_room = newRoom
+            return ""
+        else:
+            return "Error: You can't go that way."
